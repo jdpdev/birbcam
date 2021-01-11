21eefa2efa75132fe9742c7ea933130b6666fd27
jdp <gallahad@me.com>
2021-01-10 23:05:54 -0500

    find birds

--- birbloop.py
+++ birbloop.py
@@ -1,40 +1,87 @@
 from time import time, sleep
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from cv2 import imwrite, imshow, waitKey
+#from cv2 import imwrite, imshow, waitKey
+import cv2
 import sched
+import imutils
 
 WATCHER_STEP = 5
 
-def run():
-    camera = PiCamera()
-    rawCapture = PiRGBArray(camera)
-    keyframe = startup(camera, rawCapture)
+class BirbWatcher:
+    camera = None
+    rawCapture = None
+    keyframe = None
 
-    s = sched.scheduler(time, sleep)
-    s.enter(WATCHER_STEP, 1, bird_watcher, (s,))
-    s.run()
+    def __init__(self):
+        print("chirp chirp")
 
-    #imshow("Image", image)
-    #waitKey(0)
+        self.camera = PiCamera()
+        self.rawCapture = PiRGBArray(self.camera)
 
-def startup(camera, rawCapture):
-    sleep(1)
-    return capture_photo(camera, rawCapture, "startup")
+    def startup(self):
+        sleep(1)
+        return self.capture_photo("startup")
 
-def bird_watcher(sc):
-    print("looking for birbs")
-    sc.enter(WATCHER_STEP, 1, bird_watcher, (sc,))
+    def run(self):
+        self.keyframe = self.simplify_image(self.startup())
 
-def capture_photo(camera, rawCapture, save_to=None):
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
+        s = sched.scheduler(time, sleep)
+        s.enter(WATCHER_STEP, 1, self.watch_loop, (s,))
+        s.run()
 
-    # for debug
-    if not save_to is None:
-        imwrite(save_to + "-cv.jpg", image)
-        camera.capture(save_to + "-picamera.jpg")
+    def capture_photo(self, save_to=None):
+        self.rawCapture = PiRGBArray(self.camera)
+        self.camera.capture(self.rawCapture, format="bgr")
+        image = self.rawCapture.array
 
-    return image
+        # for debug
+        if not save_to is None:
+            cv2.imwrite(save_to + "-cv.jpg", image)
+            self.camera.capture(save_to + "-picamera.jpg")
 
-run()
+        return image
+
+    def simplify_image(self, image):
+        resize = imutils.resize(image, width=500)
+        gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        return gray
+
+    def compare_with_keyframe(self, image):
+        delta = cv2.absdiff(self.keyframe, image)
+        cv2.imwrite("delta.jpg", delta)
+        thresh = cv2.threshold(delta, 25, 255, cv2.THRESH_BINARY)[1]
+
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 500:
+                continue
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            #(x, y, w, h) = cv2.boundingRect(c)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #text = "Occupied"
+            return True
+
+        return False
+
+    def watch_loop(self, sc):
+        print("looking for birbs")
+
+        image = self.capture_photo()
+        simple = self.simplify_image(image)
+
+        if self.compare_with_keyframe(simple):
+            print("found one!")
+            cv2.imwrite("bird-match-cv.jpg", image)
+
+        sc.enter(WATCHER_STEP, 1, self.watch_loop, (sc,))
+
+watcher = BirbWatcher()
+watcher.run()
