4edbe61a0b3d0bec1139cf74d3e91d312af7aac9
jdp <gallahad@me.com>
2021-01-11 18:16:18 -0500

    use picamera

--- viewfinder.py
+++ viewfinder.py
@@ -1,35 +1,30 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
 import numpy as np
 
-# Create a VideoCapture object and read from input file
-# If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+previewResolution = (640, 480)
 
-# Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
+camera = PiCamera()
+camera.resolution = previewResolution
+camera.framerate = 32;
+rawCapture = PiRGBArray(camera, size=previewResolution)
 
-# Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
+time.sleep(0.1)
 
-    # Display the resulting frame
-    cv2.imshow('Frame',frame)
-
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
-
-  # Break the loop
-  else: 
-    break
-
-# When everything done, release the video capture object
-cap.release()
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
 # Closes all the frames
 cv2.destroyAllWindows()
