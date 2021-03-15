f31ee938fb818761b2ce08ce0d9186adf99ce189
jdp <gallahad@me.com>
2021-03-14 22:42:42 -0400

    cleanup and rename

--- birbcam.py
+++ birbcam.py
@@ -1,16 +1,9 @@
 from picamerax.array import PiRGBArray
 from picamerax import PiCamera
-from time import time, sleep
-import common
+from time import sleep
 import lensshading
-import cv2
-import numpy as np
-import imutils
-import argparse
 import sys
 import logging
-import sched
-from datetime import datetime
 from setproctitle import setproctitle
 
 from birbconfig import BirbConfig
@@ -18,22 +11,7 @@ from focusassist import FocusAssist
 from imagemask import ImageMask
 from birbwatcher import BirbWatcher
 
-LIVE_CAMERA_STEP = 10
-FULL_PICTURE_STEP = 10
-
 previewResolution = (640, 480)
-#FULL_RES = (3280, 2464)
-FULL_RES = (4056, 3040)
-windowName = 'birbcam'
-saveLocation = '/home/pi/Public/birbs/'
-debugMode = False
-noCaptureMode = False
-average = None
-scheduler = sched.scheduler(time, sleep)
-takeFullPicture = False
-takeLivePicture = False
-nextLivePictureTime = 0
-nextFullPictureTime = 0
 
 setproctitle("birbcam")
 config = BirbConfig()
@@ -68,18 +46,6 @@ camera.meter_mode = 'spot'
 sleep(2)
 
 camera.shutter_speed = camera.exposure_speed
-#camera.exposure_mode = 'off'
-#g = camera.awb_gains
-#camera.awb_mode = 'auto'
-#camera.awb_gains = g
-
-if debugMode:
-    logging.info("Using camera settings...")
-    logging.info("  ISO: %d", camera.iso)
-    logging.info("  Metering: " + camera.meter_mode)
-    logging.info("  Exposure Mode: " + camera.exposure_mode)
-    logging.info("  White Balance: " + camera.awb_mode)
-    logging.info("  Shutter: %d", camera.shutter_speed)
 
 # **************************************
 #   Focus assist
