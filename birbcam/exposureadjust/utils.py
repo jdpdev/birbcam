b00f5eaaf8299f8baa52a64dda261107959e7801
jdp <gallahad@me.com>
2021-03-23 11:15:29 -0400

    fix var change

--- birbcam/exposureadjust/utils.py
+++ birbcam/exposureadjust/utils.py
@@ -2,7 +2,7 @@ from cv2 import cvtColor, calcHist, COLOR_BGR2GRAY
 import numpy as np
 
 def calculate_exposure(image):
-    histogram = calcHist([luminance], [0], None, [256], [0,255])
+    histogram = calcHist([image], [0], None, [256], [0,255])
     data = np.int32(np.around(histogram))
 
     max = data.max()
