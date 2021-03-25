aa1d6a57484c940d587a3a0e4da7d106a867952d
jdp <gallahad@me.com>
2021-03-24 21:56:03 -0400

    fix tests

--- tests/sleep_test.py
+++ tests/sleep_test.py
@@ -9,14 +9,14 @@ def test_time_to_update():
         assert state == None
 
     sleep = Sleep(50)
-    sleep.take_over(None, None, mockChangeState, 100, 10)
+    sleep.take_over(None, None, None, mockChangeState, 100, 10)
     sleep.update(None, None)
 
 @patch('time.time', MagicMock(return_value=25))
 def test_not_time_to_update():
     mockChangeState = Mock()
     sleep = Sleep(50)
-    sleep.take_over(None, None, mockChangeState, 100, 10)
+    sleep.take_over(None, None, None, mockChangeState, 100, 10)
     sleep.update(None, None)
 
     assert not mockChangeState.called
